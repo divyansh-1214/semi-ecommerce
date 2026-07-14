605966baad47c365ef4476f80a7d02b7fd2a3e21
divyansh-1214 <divyansh.srri23@gmail.com>
2026-07-15 03:59:20 +0530

    idk

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-
+import env from "dotenv";
 const rawBaseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-const baseURL = rawBaseURL
-  ? rawBaseURL.replace(/['"]/g, "").trim()
-  : "http://localhost:5000";
-
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
 export const api = axios.create({
   baseURL,
   timeout: 30000,
