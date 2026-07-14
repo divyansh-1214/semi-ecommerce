08ea991c18f4c801ec4c86be5db896f71c734b3b
divyansh-1214 <divyansh.srri23@gmail.com>
2026-07-15 03:21:56 +0530

    feat: initialize express backend server with Vercel deployment configuration and dependency setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,16 +29,20 @@ app.use("/api", routes);
 app.use(errorHandler);
 
 
-async function startServer() {
-  try {
-    await prisma.$connect();
-    console.log("Connected to the database successfully");
+if (!process.env.VERCEL) {
+  async function startServer() {
+    try {
+      await prisma.$connect();
+      console.log("Connected to the database successfully");
+    }
+    catch (error) {
+      console.error("Failed to connect to the database:", error);
+    }
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("server is stated at http://localhost:5000");
+    })
   }
-  catch (error) {
-    console.error("Failed to connect to the database:", error);
-  }
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("server is stated at http://localhost:5000");
-  })
+  startServer()
 }
-startServer()
+
+export default app;
